d5eb42d2ac2596b078c721a673abb1f8366b0aa7
PetraNemcekova <101061242+PetraNemcekova@users.noreply.github.com>
2022-03-11 23:10:57 +0100

    Final PLS

--- PLS.py
+++ PLS.py
@@ -1,10 +1,8 @@
 import numpy as np
 import pandas as pd
 import functions as fcn
-import matplotlib.pyplot as plt
 from sklearn.cross_decomposition import PLSRegression 
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
 
 
@@ -16,7 +14,7 @@ met_arr, gen_arr, gen_feature_names = fcn.PrepareForPrediction(met_preprocessed,
 
 # deviding into training and testing set
 
-met_train, met_test, gen_train, gen_test = train_test_split(met_arr, gen_arr, test_size = 0.4,random_state=0)
+met_train, met_test, gen_train, gen_test = train_test_split(met_arr, gen_arr, test_size = 0.3,random_state=0)
 met_train = np.delete(met_train,7, axis = 1)
 met_test = np.delete(met_test,7, axis = 1)
 
@@ -31,9 +29,22 @@ met_pred = pls.predict(gen_test)
 met_test = pd.DataFrame(met_test, columns=['MET12', 'MET21', 'MET25', 'MET38', 'MET39', 'MET66', 'MET77', 'MET88', 'MET102', 'MET111', 'MET119', 'MET122'])
 met_pred = pd.DataFrame(met_pred, columns=['MET12', 'MET21', 'MET25', 'MET38', 'MET39', 'MET66', 'MET77', 'MET88', 'MET102', 'MET111', 'MET119', 'MET122'])
 
-mse = mean_squared_error(met_test, met_pred) 
-mae = mean_absolute_error(met_test, met_pred)  
+model_mse = mean_squared_error(met_test, met_pred) 
+model_mae = mean_absolute_error(met_test, met_pred)  
 
-corr_matrix = np.corrcoef(met_test.T, met_pred.T)[0:12, 12:24]
-metabolites_correlations = np.mean(corr_matrix, axis = 1)
 
+columns=['MET12', 'MET21', 'MET25', 'MET38', 'MET39', 'MET66', 'MET77', 'MET88', 'MET102', 'MET111', 'MET119', 'MET122']
+maes = []
+mses = []
+
+for i in columns:
+    mae = mean_absolute_error(met_test[i], met_pred[i])  
+    maes.append(mae)
+    mse = mean_squared_error(met_test[i], met_pred[i])  
+    mses.append(mse)
+
+sd_mae = np.std(maes)
+sd_mse = np.std(mses)
+
+print(model_mae, sd_mae)
+print(model_mse, sd_mse)
